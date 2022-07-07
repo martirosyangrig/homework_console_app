142ff9fe1cc9ec17a7556a366352827986229731
martirosyangrig <grigor.martirosyan2000@mail.ru>
2022-07-07 19:25:22 +0300

    homework

--- js/filmapp.js
+++ js/filmapp.js
@@ -1,76 +1,88 @@
 "use strict";
 
-let numberOfFilms;
-function isInt() {
-	numberOfFilms = +prompt("Քանի՞ ֆիլմ եք դիտել այսօր", "");
+const numberOfFilms = start();
 
-	while (true) {
-        if(numberOfFilms == null  || numberOfFilms == "" || isNaN(numberOfFilms)){
-            numberOfFilms = +(prompt("Քանի՞ ֆիլմ եք դիտել այսօր", ""));
-        }
-		else{
-            break;
-        }
-	}
-}
-isInt();
 const personalMovieDB = {
-	count: numberOfFilms >= 1 ? numberOfFilms : 0,
+	count: numberOfFilms,
 	movies: {},
 	actors: {},
 	genres: [],
 	privat: true,
 
-    showMyDB(){
-        if(personalMovieDB.privat === true){
-            console.log(personalMovieDB);
-        }else{
-            console.log("knereq chneq karox cuyc tal mer db");
-        }
-    },
+	
+	rememberMyFilms () {
+		for (let i = 0; i < 2; i++) {
+			const a = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել", "");
+			const b = prompt("Ինչքա՞ն այդ ֆիլմը կգնահատեիք", "");
+		
+			if (!isNaN(b) && a != null && b != null && a.trim() != "" && b.trim() != "" && a.length < 50) {
+				personalMovieDB.movies[a] = b;
+				console.log("DONE");
+			} else {
+				console.log("ERROR");
+				i--;
+			}
+		}
+	},
+	detectUserPersonalLevel () {
+		if (numberOfFilms < 10) {
+			console.log("Դուք նայել եք բավականին քիչ ֆիլմեր");
+		} else if (numberOfFilms < 30) {
+			console.log("Դուք ֆիլմի սիրահար եք");
+		} else if (numberOfFilms >= 30) {
+			console.log("Դուք կինոման եք !!!");
+		} else {
+			console.log("Տեղի է ունեցել խնդիր, ըստ երևույթի դուք թիվ չեք նշել");
+		}
+	},
+	yourFavoriteGenres () {
+		for (let i = 0; i <= 2; i++) {
+			const genres = prompt(`Ձեր նախընտրելի ժանրը ${i + 1}`);
+	
+			if (genres != null && genres.trim() != "" && genres.length <= 20) {
+				personalMovieDB.genres[i] = genres;
+			} else {
+				i--;
+			}
+		}
+		personalMovieDB.genres.forEach(function a(val, i){
+			console.log(`dzer sireli janere ${i+1} -- ${val}`);
+		});
 
-    yourFavoriteGenres() {
-        for(let i = 0; i <= 2; i++){
-            const x = prompt(`« Ձեր նախընտրելի ժանրը» ${i + 1}`);
-            
-            if(isNaN(x) && x != null && x != "" && x.length <= 20){
-                personalMovieDB.genres[i] = x;
-            }else{
-                console.log("sxal e texi unecel, mutqagreq bar");
-                i--;
-            }
-        }
-    }
+	},
+	showMyDB () {
+		if (personalMovieDB.privat) {
+			console.log(personalMovieDB);
+		} else {
+			console.log("Մեր տվյալների բազան փակ է");
+		}
+	},
+	isVisibleMyDB(){
+		personalMovieDB.privat === true ? personalMovieDB.privat = false : personalMovieDB.privat = true;
+	},
+	
+
 };
-personalMovieDB.yourFavoriteGenres();
 
+const{detectUserPersonalLevel, rememberMyFilms, yourFavoriteGenres,	showMyDB, isVisibleMyDB} = personalMovieDB;
 
-	if (numberOfFilms < 10) {
-		console.log("Դուք նայել եք բավականին քիչ ֆիլմեր");
-	} else if (numberOfFilms < 30) {
-		console.log("Դուք ֆիլմի սիրահար եք");
-	} else if (numberOfFilms >= 30) {
-		console.log("Դուք կինոման եք !!!");
-	} else {
-		console.log("Տեղի է ունեցել խնդիր, ըստ երևույթի դուք թիվ չեք նշել");
+function start (numberOfFilms) {
+	numberOfFilms = parseInt(prompt("Քանի՞ ֆիլմ եք դիտել այսօր", ""));
+	
+	while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms) || numberOfFilms < -1) {
+		numberOfFilms = parseInt(prompt("Քանի՞ ֆիլմ եք դիտել այսօր", ""));
 	}
 
-function myFilms(){
-    for (let i = 0; i < 2; i++) {
-        const a = prompt("Ո՞ր ֆիլմն եք վերջերս դիտել", "");
-        const b = prompt("Ինչքա՞ն այդ ֆիլմը կգնահատեիք");
-    
-        if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-            a.trim();
-            b.trim();
-            personalMovieDB.movies[a] = b;
-            console.log("DONE");
-        } else {
-            console.log("ERROR");
-            i--;
-        }
-    }
+	return numberOfFilms;
 }
 
-myFilms();
-personalMovieDB.showMyDB();
+detectUserPersonalLevel();
+rememberMyFilms ();
+yourFavoriteGenres();
+isVisibleMyDB();
+showMyDB();
+
+
+
+
+
